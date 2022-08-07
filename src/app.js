8a8dd2990d10774b16e5166b8ba94a155fcf71cd
Abhik Ray <abhikray@Abhiks-MacBook-Air.local>
2022-08-07 16:23:54 -0400

    before testing

--- src/app.js
+++ src/app.js
@@ -1,18 +1,59 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
-
+import Realm from 'realm';
+import { Server } from 'socket.io';
+import { createServer } from 'https';
+const io = new Server(httpServer, {
+	/* options */
+});
 const app = express();
 
 // Apply middlware for CORS and JSON endpoing
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+io.on('connection', (socket) => {
+	console.log(socket.id);
+	socket.on('data-request', (room) => {
+		// join the room, find the document
+		// send the document, delete the document
+		socket.join(room);
+		const documents = realm.objects('Document');
+		const matchingDocument = documents.filtered(`_id = '${room}'`);
+		socket.to(room).emit('documents', matchingDocument);
+		realm.delete(matchingDocument);
+	});
 });
+const DocumentSchema = {
+	name: 'Document',
+	properties: {
+		_id: 'string',
+	},
+	primaryKey: '_id',
+};
+app.get('/', (req, res) => {
+	res.send('Hello World!');
+});
+app.post('/data', (req, res) => {
+	const { id, data, schema } = req.params;
+	// write data to realm with id tag
+	realm.write(() => {
+		realm.create('Document', {
+			_id: id,
+			data: data,
+			schema: schema,
+		});
+	});
+	// emits to the room with the thing
+	// get a response from angular client that we can delete the data safely
+});
+const httpServer = createServer(app);
 
-app.listen(process.env.PORT, () =>
-  console.log('Example app listening on port 3000!'),
-);
+httpServer.listen(process.env.PORT, async () => {
+	console.log('Example app listening on port 3000!');
+	const realm = await Realm.open({
+		path: 'myrealm',
+		schema: [DocumentSchema],
+	});
+});
